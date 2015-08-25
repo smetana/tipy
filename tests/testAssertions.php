6789b27244896fe2b7e667983b5da103f01db191
Serge Smetana <serge.smetana@gmail.com>
2015-08-26 01:15:19 +0300

    Added test for assertions.

---
+++ tests/testAssertions.php
@@ -0,0 +1,58 @@
+<?php
+
+require_once 'autoload.php';
+
+
+class TestAssertions extends TipyTestSuite {
+    
+    public function testEqual() {
+        $this->assertEqual(1, 1);
+        $this->assertEqual("1", 1);
+        $this->assertEqual(true, 1);
+        $this->assertEqual(new DateTime("2015-08-26"), new DateTime("2015-08-26"));
+    }
+
+    public function testSame() {
+        $this->assertSame(1, 1);
+        $this->assertSame("1", "1");
+        $this->assertSame(false, false);
+    }
+
+    public function testNotEqual() {
+        $this->assertNotEqual(1, "2");
+        $this->assertNotEqual(1, 2);
+        $this->assertNotEqual(1, false);
+        $this->assertNotEqual(new DateTime("2015-08-26"), new DateTime("2015-08-25"));
+    }
+
+    public function testNull() {
+        $this->assertNull(null);
+    }
+
+    public function testNotNull() {
+        $this->assertNotNull(0);
+        $this->assertNotNull('');
+        $this->assertNotNull(false);
+    }
+
+    public function testTrue() {
+        $this->assertTrue(true);
+    }
+
+    public function testFalse() {
+        $this->assertFalse(false);
+    }
+
+    public function testThrown() {
+        $this->assertThrown("Exception", "Bang!", function() {
+            throw new Exception("Bang!");
+        });
+    }
+
+    public function testThrownRegexp() {
+        $this->assertThrownRegexp("Exception", "/really/", function() {
+            throw new Exception("Bang! It is a really Bang!");
+        });
+    }
+
+}
