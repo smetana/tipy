5dc56915de42c0761739873aa96eea8f2e16a8b2
Serge Smetana <serge.smetana@gmail.com>
2015-09-02 02:17:43 +0300

    Documentation

--- src/TipyOutput.php
+++ src/TipyOutput.php
@@ -1,12 +1,26 @@
 <?php
+/**
+ * TipyOutput
+ *
+ * @package tipy
+ */
 
-// ==================================================================
-// Output
-// ==================================================================
-
+/**
+ * Action output data.
+ *
+ * These values will be assigned to {@link TipyView} template on page rendering
+ *
+ * Usage:
+ * <code>
+ * class MyController extends TipyController {
+ *     public function index() {
+ *         $this->out->set('firstName', 'John');
+ *         // or by shortcut
+ *         $this->out('lastName', 'Doe');
+ *     }
+ * }
+ * </code>
+ * @see TipyView
+ */
 class TipyOutput extends TipyIOWrapper {
-
-    public function __construct() {
-        parent::__construct();
-    }
 }
--- src/TipyView.php
+++ src/TipyView.php
@@ -1,88 +1,160 @@
 <?php
+/**
+ * TipyView
+ *
+ * @package tipy
+ */
 
-// ==================================================================
-// View class
-// Template engine based on php itself and it's output bufer
-// ==================================================================
-
+/**
+ * HTML template engine based on PHP output buffer and PHP itself
+ *
+ * Usage:
+ * <code>
+ * // app/controllers/HelloWorldController.php
+ * class HelloWorldController extends TipyController {
+ *     public function article() {
+ *         $this->out('title', 'Hello');
+ *         $this->out('message', 'World!');
+ *         $this->renderView('helloWorld');
+ *     }
+ * }
+ * </code>
+ *
+ * <code>
+ * // app/views/helloWorld.php
+ * <!DOCTYPE html>
+ * <html>
+ * <head>
+ *     <title><?= $title ></title>
+ * </head>
+ * <body>
+ *     <p><?= $title.' '.$message ?></p>
+ * </body>
+ * </html>
+ * </code>
+ *
+ * You can wrap one template in another one
+ *
+ *
+ */
 class TipyView {
 
-    private $assigns; // Stores variable assigned to template
+    /**
+     * variables assigned to templates
+     * @internal
+     */
+    private $assigns;
+
+    /**
+     * absolute path to template files
+     * @internal
+     */
     private $templatePath;
 
-    // This two stacks are used when processed template (or the part)
-    // is nested into other template.
-    // See applyTemplateStart and applyTemplateEnd below for details
+    /**
+     * These two stacks are used when processed template (or its part)
+     * is nested into other template.
+     * @see TipyView::applyTemplateStart()
+     * @see TipyView::applyTemplateEnd()
+     * @internal
+     */
     private $templateStack;
     private $contentStack;
 
-    // --------------------------------------------------------------
-    // Contrustor
-    // --------------------------------------------------------------
+
     public function __construct() {
         $this->assigns          = new TipyIOWrapper();
         $this->contentStack     = [];
         $this->templateStack    = [];
     }
 
-    // --------------------------------------------------------------
-    // Bind array to assigns
-    // --------------------------------------------------------------
-    public function bind($data) {
-        $this->assigns->bind($data);
+    /**
+     * Assign variables map to template.
+     * This will replace all output data.
+     */
+    public function bind(array $map) {
+        $this->assigns->bind($map);
     }
 
-    // --------------------------------------------------------------
-    // Get assigned variable
-    // --------------------------------------------------------------
-    public function get($varname) {
+    /**
+     * Get value of the variable assigned to template by variable name.
+     * If $key does not exists may return $defaultValue.
+     *
+     * @param string $key
+     * @param mixed $defaultValue
+     * @return mixed
+     */
+    public function get($key) {
         if (func_num_args() > 1) {
-            return $this->assigns->get($varname, $func_get_arg(1));
+            return $this->assigns->get($key, $func_get_arg(1));
         } else {
-            return $this->assigns->get($varname);
+            return $this->assigns->get($key);
         }
     }
 
-    // --------------------------------------------------------------
-    // Assign variable to template
-    // --------------------------------------------------------------
-    public function set($varname, $value) {
-        $this->assigns->set($varname, $value);
+    /**
+     * Assign variable to template
+     *
+     * @param string $key
+     * @param mixed $value
+     */
+    public function set($key, $value) {
+        $this->assigns->set($key, $value);
     }
 
-    // --------------------------------------------------------------
-    // Get all assigned variables
-    // --------------------------------------------------------------
+    /**
+     * Get all assigned variables
+     *
+     * @return array
+     */
     public function getAll() {
         return $this->assigns->getAll();
     }
 
-    // --------------------------------------------------------------
-    // Set path to templates
-    // --------------------------------------------------------------
+    /**
+     * Set path to templates
+     *
+     * @param string $path
+     */
     public function setTemplatePath($path) {
         $this->templatePath = $path;
     }
 
-    // --------------------------------------------------------------
-    // Process template
-    // --------------------------------------------------------------
+    /**
+     * Compile template and return result as a string
+     *
+     * @param string $templateName
+     * @return string
+     */
     public function processTemplate($templateName) {
-        $templateName = $this->templateName($templateName);
+        $templateFile = $this->expandTemplatePath($templateName);
         $vars = $this->assigns->getAll();
         extract($vars);
         $output = "";
         ob_start();
-        include($templateName);
+        include($templateFile);
         $output = ob_get_clean();
         return $output;
     }
 
+    /**
+     * Get full path to template file
+     *
+     * @param string $templateName
+     * @return string
+     */
+    public function expandTemplatePath($templateName) {
+        return $this->templatePath . "/" . $templateName . ".php";
+    }
+
     // --------------------------------------------------------------
-    // Expand template name with path
+    // include template
     // --------------------------------------------------------------
-    public function templateName($name) {
-        return $this->templatePath . "/" . $name . ".php";
+    private function includeTemplate($templateName) {
+        $templateFile = $this->expandTemplatePath($templateName);
+        $vars = $this->assigns->getAll();
+        extract($vars);
+        include($templateFile);
     }
 
     // --------------------------------------------------------------
@@ -125,13 +197,4 @@ class TipyView {
         }
     }
 
-    // --------------------------------------------------------------
-    // include template
-    // --------------------------------------------------------------
-    private function includeTemplate($templateName) {
-        $templateName = $this->templateName($templateName);
-        $vars = $this->assigns->getAll();
-        extract($vars);
-        include($templateName);
-    }
 }
