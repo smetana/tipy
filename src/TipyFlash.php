512c90daf3a90a8f2c1753553a52df1149477ad4
Serge Smetana <serge.smetana@gmail.com>
2015-09-04 02:37:49 +0300

    Documented TipyFlash, TipyRequest

--- src/TipyFlash.php
+++ src/TipyFlash.php
@@ -1,20 +1,61 @@
 <?php
+/**
+ * TipyFlash
+ *
+ * @package tipy
+ */
 
+/**
+ * Send data between actions
+ *
+ * The flash concept is borrowed from Rails. It provides a way to pass
+ * temporary primitive-types (Strings, Integers, Arrays) between actions.
+ * Anything you place in the flash will be exposed to the next action.
+ * This is a great way of doing notices and alerts after redirects.
+ *
+ * <code>
+ * class BlogController extends TipyController {
+ *     public function save() {
+ *         // ...save post...
+ *         $this->flash->set('Post successfully created');
+ *         $this->redirect('/blog/index');
+ *     }
+ * }
+ * </code>
+ */
 class TipyFlash {
 
+    /**
+     * Flash keeps its data in session
+     * @internal
+     */
     private $session;
+    /**
+     * currentMessage is available only or current action
+     * and will be cleared on the very next action
+     * @internal
+     */
     private $currentMessage;
 
+    /**
+     * @param TipySession $session
+     */
     public function __construct($session) {
         $this->session = $session;
         $this->currentMessage = $this->session->get('flashMessage');
         $this->session->set('flashMessage', null);
     }
 
-    public function set($value) {
-        $this->session->set('flashMessage', $value);
+    /**
+     * @param mixed $message
+     */
+    public function set($message) {
+        $this->session->set('flashMessage', $message);
     }
 
+    /**
+     * @return mixed
+     */
     public function get() {
         return $this->currentMessage;
     }
--- src/TipyRequest.php
+++ src/TipyRequest.php
@@ -1,5 +1,16 @@
 <?php
+/**
+ * TipyRequest
+ *
+ * @package tipy
+ */
 
+/**
+ * Access request and its header
+ *
+ * By default it is a TipyIOWrapper around $_SERVER superglobal
+ * @see TipyIOWrapper
+ */
 class TipyRequest extends TipyIOWrapper {
 
     /**
@@ -7,21 +18,40 @@ class TipyRequest extends TipyIOWrapper {
      */
     public function __construct() {
         parent::__construct();
+        // little hack to autocreate $_SERVER if
+        // auto_globals_jit is on
+        $foo = $_SERVER['PHP_SELF'];
         $this->bind($_SERVER);
     }
 
+    /**
+     * Return request method: GET,POST,DELETE,etc...
+     * @return string
+     */
     public function method() {
         return $this->get('REQUEST_METHOD');
     }
 
+    /**
+     * Return true if request is a GET request
+     * @return boolean
+     */
     public function isGet() {
         return $this->method == 'GET';
     }
 
+    /**
+     * Return true if request is a POST request
+     * @return boolean
+     */
     public function isPost() {
         return $this->method == 'POST';
     }
 
+    /**
+     * Returns true if request is AJAX request
+     * @return boolean
+     */
     public function isXhr() {
         return $this->get('HTTP_X_REQUESTED_WITH') == 'XMLHttpRequest';
     }
